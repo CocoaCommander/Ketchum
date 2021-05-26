2db26c3e0738f4491c2aa3a87854694ead8a1be8
Ryan Lee <ralee478.school@gmail.com>
2021-05-26 15:34:20 -0700

    Initial Commit

---
+++ Ketchum/KetchumApp.swift
@@ -0,0 +1,17 @@
+//
+//  KetchumApp.swift
+//  Ketchum
+//
+//  Created by Ryan Lee on 5/26/21.
+//
+
+import SwiftUI
+
+@main
+struct KetchumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
