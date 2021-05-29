a48cc225fc110ded4fd1ba7d561359d94764bb75
“vivianngu3” <vn23@uw.edu>
2021-05-28 20:33:35 -0700

    splashscreen & fonts

--- Ketchum/ContentView.swift
+++ Ketchum/ContentView.swift
@@ -8,9 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var animate = false
+    @State var endSplash = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            VStack (spacing: 25) {
+                Text("Welcome to")
+                    .font(.custom("ExtraLight", size: 24))
+                    .scaleEffect(animate ? 3 : 1)
+                Image("pokeball")
+                    .resizable()
+                    .renderingMode(.original)
+                    .aspectRatio(contentMode: animate ? .fill : .fit)
+                    .frame(width: animate ? nil : 100, height: animate ? nil : 100)
+                    
+                    .scaleEffect(animate ? 3 : 1)
+                    .frame(width: UIScreen.main.bounds.width)
+            
+                Text("Ketchum")
+                    .font(.custom("Bold", size: 30))
+                    .fontWeight(.medium)
+                    .scaleEffect(animate ? 3 : 1)
+                    
+            }
+            .ignoresSafeArea(.all, edges: .all)
+            .onAppear(perform: {animateSplash()})
+            .opacity(endSplash ? 0 : 1)
+        }
+}
+    
+    func animateSplash(){
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.25) { withAnimation(Animation.easeOut(duration: 1)){
+                animate.toggle()
+            }
+        withAnimation(Animation.linear(duration: 0.8)){
+                endSplash.toggle()
+            }
+        }
     }
 }
 
@@ -19,3 +55,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
