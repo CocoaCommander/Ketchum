92502afa6dffbd6ac865b10ab52478c9c71e6a34
limj27 <limjustin2@gmail.com>
2021-06-03 20:17:57 -0700

    Merge Conflict

--- Ketchum/ContentView.swift
+++ Ketchum/ContentView.swift
@@ -12,8 +12,7 @@ struct ContentView: View {
     @State var index = 0
     
     var body: some View {
-<<<<<<< HEAD
-        
+    
         VStack{
             ZStack {
                 if self.index == 0 {
@@ -42,27 +41,6 @@ struct ContentView: View {
                     Text("My Deck")
                         .font(.custom("Bold", size: 30))
                         .foregroundColor(Color.white)
-=======
-        ZStack {
-            VStack (spacing: 25) {
-                Text("Welcome to")
-                    .font(.custom("ExtraLight", size: 24))
-                    .scaleEffect(animate ? 3 : 1)
-                Image("pokeball")
-                    .resizable()
-                    .renderingMode(.original)
-                    .aspectRatio(contentMode: animate ? .fill : .fit)
-                    .frame(width: animate ? nil : CGFloat(100), height: animate ? nil : CGFloat(100))
-                    
-                    .scaleEffect(animate ? CGFloat(3) : CGFloat(1))
-                    .frame(width: UIScreen.main.bounds.width)
-            
-                Text("Ketchum")
-                    .font(.custom("Bold", size: 30))
-                    .fontWeight(.medium)
-                    .scaleEffect(animate ? CGFloat(3) : CGFloat(1))
->>>>>>> 653aebb6be29871bdfdbf32e7970593bddddb5a4
-                    
                 }
                Welcome()
             }
